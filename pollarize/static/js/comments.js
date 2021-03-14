3bf1d115f5fe88569aee03af48e4ea7951b39381
RobbertSinclair <christanina57@gmail.com>
2021-03-14 11:54:15 +0000

    Added javascript to allow the user to load replies

--- pollarize/static/js/comments.js
+++ pollarize/static/js/comments.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
     alert("Hello World");
-    $("#load-replies").click(function(){
-        childCommentRequest = new XMLHttpRequest();
-    })
 });
+
+function loadReplies(comment_id) {
+    var get_replies = new XMLHttpRequest();
+    console.log("loadReplies has been run");
+    $.get("/json/" + comment_id + "/child-comments", function( data ) {
+        console.log(data);
+        for (var i = 0; i < data.comments.length; i++) {
+            var the_comment = data.comments[i];
+            console.log("the_comment")
+            $("#replies-" + comment_id).append("<div class='comment' id='comment-" + the_comment.id + 
+            "'><h3>" + the_comment.submitter + "</h3><p>" + the_comment.comment + "</p></div>");
+            $("#load-replies-"+ comment_id).remove();
+        }
+    });
+}
