40cf191cc08a8d3a53fdfec0271ed089355e73c2
RobbertSinclair <christanina57@gmail.com>
2021-03-17 01:03:45 +0000

    Added a load and hide replies feature to the comments

--- pollarize/static/js/comments.js
+++ pollarize/static/js/comments.js
@@ -1,20 +1,34 @@
 $(document).ready(function(){
     $(".loading").hide();
+    $(".replies").hide();
+    $(".hide-replies").hide();
 })
 
 
 function loadReplies(comment_id) {
+    var reply_children_count = $("#replies-" + comment_id).children().length;
+    
     $("#loading-" + comment_id).show();
-    console.log("loadReplies has been run");
-    $.get("/json/" + comment_id + "/child-comments", function( data ) {
-        for (var i = 0; i < data.comments.length; i++) {
-            var the_comment = data.comments[i];
-            $("#replies-" + comment_id).append("<div class='comment' id='comment-" + the_comment.id + 
-            "'><img class='mr-3 rounded-circle profile-img' alt='Profile image' src='" + the_comment.profile_image + "'/><h3>" + the_comment.submitter + "</h3><p>" + the_comment.comment + "</p></div>");
-            $("#load-replies-"+ comment_id).remove();
-        }
-    });
+    if (reply_children_count == 1) {
+        $.get("/json/" + comment_id + "/child-comments", function( data ) {
+            for (var i = 0; i < data.comments.length; i++) {
+                var the_comment = data.comments[i];
+                $("#replies-" + comment_id).append("<div class='comment' id='comment-" + the_comment.id + 
+                "'><img class='mr-3 rounded-circle profile-img' alt='Profile image' src='" + the_comment.profile_image + "'/><h3>" + the_comment.submitter + "</h3><p>" + the_comment.comment + "</p></div>");
+                
+            } 
+        });
+    }
+    $("#load-replies-"+ comment_id).hide();
+    $("#hide-replies-" + comment_id).show();
+    $("#replies-" + comment_id).slideDown("slow");
     $('#loading-' + comment_id).hide();
+}
+
+function hideReplies(comment_id) {
+    $("#replies-" + comment_id).slideUp("slow");
+    $("#load-replies-" + comment_id).show();
+    $("#hide-replies-" + comment_id).hide();
 }
 
 
