c036b7ef45a9e25debde7f11282925ad23dc1956
RobbertSinclair <christanina57@gmail.com>
2021-03-20 11:21:59 +0000

    Added styling for the back to top button

--- pollarize/static/js/comments.js
+++ pollarize/static/js/comments.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 function loadReplies(comment_id) {
     var reply_children_count = $("#replies-" + comment_id).children().length;
     $("#replies-" + comment_id).slideDown("slow");
-    $("#loading-" + comment_id).slideDown("slow");
+    $("#loading-" + comment_id).show();
     if (reply_children_count == 1) {
         $.get("/json/" + comment_id + "/child-comments", function( data ) {
             for (var i = 0; i < data.comments.length; i++) {
@@ -20,14 +20,14 @@ function loadReplies(comment_id) {
                 $("#replies-" + comment_id).append("<div class='comment' id='comment-" + the_comment.id + 
                 "'><img class='mr-3 rounded-circle profile-img' alt='Profile image' src='" + the_comment.profile_image + "'/><h3>" + the_comment.submitter + "</h3><p>" + the_comment.comment + "</p></div>");
                 
-            } 
+            }
         });
         
     }
     $("#load-replies-"+ comment_id).hide();
     $("#hide-replies-" + comment_id).show();
     
-    $('#loading-' + comment_id).slideUp("slow");
+    $('#loading-' + comment_id).hide();
 }
 
 function hideReplies(comment_id) {
