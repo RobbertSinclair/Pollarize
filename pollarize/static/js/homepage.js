c371af0e8e80e97995b28d460cefb6092a1b80fd
Adam Fairlie <2461352f@student.gla.ac.uk>
2021-03-25 18:12:13 +0000

    cleaned up and commented JS code

--- pollarize/static/js/homepage.js
+++ pollarize/static/js/homepage.js
@@ -1,38 +1,48 @@
 $(document).ready(function(){
     console.log("homepage.js loaded successfully");
-
+    //populate all poll boxes
     $(".poll_box").each(function() {
     populateBox(this)
     });
 
 })
-
+//Update data for poll box
 function populateBox(box) {
+    //get poll slug from box
     var poll_slug = $(box).attr('id').substring(5)
-    console.log(poll_slug);
 
+    //get results of poll from AJAX
     $.get("/json/" + poll_slug + "/results", function( data ) {
+           //get total votes and percentage split
            var total = data.votes1 + data.votes2;
            var percent1 = Math.round((data.votes1 / total) * 100);
            var percent2 = 100 - percent1;
+
+           //update percentages
            $(box).find('.orange_percent').html(percent1 + '%');
            $(box).find('.grey_percent').html(percent2 + '%');
-           $(box).find('.box_votes').html(total + ' votes');
 
+           //calculate vote string
            var totalString;
            if(total < 1000){
             totalString = total + " votes";
            }
            else if(total < 1000000){
+             //display in terms of thousands with one decimal
              totalString = (Math.round((total / 100)) / 10) + "K votes";
            } else{
+             //display in terms of millions with one decimal
              totalString = (Math.round((total / 100000)) / 10) + "M votes";
            }
+           //set vote string
            $(box).find('.box_votes').html(totalString);
 
+           //change vote bar to match percentages
            var orange_bar_start = Math.min(Math.max(percent1, 1), 99)
-           console.log(orange_bar_start);
            $(box).find('.vote_bar').css("background",
            "linear-gradient(90deg, #ff9c00 " + orange_bar_start + "%, #505050 " + (orange_bar_start + 0.1) + "%)");
+
+           //log progress
+           console.log("Updated data for question " + poll_slug)
         });
 }
