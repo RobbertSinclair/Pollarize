e6b0d4c9c0fad9034c97a8a643f5b21ece950543
Adam Fairlie <2461352f@student.gla.ac.uk>
2021-03-30 06:05:51 +0100

    increased variability of balloon timings for cleaner animation

--- pollarize/static/js/balloons.js
+++ pollarize/static/js/balloons.js
@@ -3,7 +3,7 @@ var balloons_on = false;
 var balloons_on_load = 20;
 //Variables to alter boundaries of randomness
 var min_time = 125; var dev_time = 300;
-var min_ms = 650; var dev_ms = 1250
+var min_ms = 650; var dev_ms = 2000;
 //Size of padding on left and right side where balloons can't generate
 var padding = 100;
 
